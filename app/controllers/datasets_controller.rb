d5e0b9e3f655d4a8cfcf9fd9c0a5b5bd6f2084d6
Gwynne Noth <nothgwynne@gmail.com>
2017-09-29 21:16:24 -0500

    Attempt debugging of d3 and jquery issues

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -12,6 +12,28 @@ class DatasetsController < ApplicationController
 
     @numeric_objects = [@debts]
     @numeric_titles = ['Debts']
-
   end
+
+  def query
+    @departments = TrumpAdminDebts::Department.all
+    @employees = TrumpAdminDebts::Employee.all
+    @debts = TrumpAdminDebts::Debt.all
+    @debt_types = TrumpAdminDebts::DebtType.all
+    @lenders = TrumpAdminDebts::Lender.all
+
+    if params[:descriptor] == "departments"
+      @descriptor = @department.to_json
+    elsif params[:descriptor] == "employees"
+      @descriptor = @employees.to_json
+    elsif params[:descriptor] == "debt_types"
+      @descriptor = @debt_types.to_json
+    elsif params[:descriptor] == "lenders"
+      @descriptor = @lenders.to_json
+    end
+
+    respond_to do |format|
+      format.text {render json: @descriptor}
+    end
+  end
+
 end
