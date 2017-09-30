c1449d4543c4cf887ad7272034cf40c963920c83
Gwynne Noth & Ida Lu <pair+genoth.idaylu@devbootcamp.com>
2017-09-30 18:28:29 -0500

    Fix multiple appends of visualizations. Move form above chart.

--- app/assets/javascripts/pie-chart.js
+++ app/assets/javascripts/pie-chart.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
     method: $form.attr("method")
   })
   $request.done(function(serverResponse){
+    $("#chart").empty();
     renderPieChart(serverResponse)
     renderDownloadButton();
   })
