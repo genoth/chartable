8f48e4dd8fe37322c800299ed65b7f8e53806ae6
Gwynne Noth <nothgwynne@gmail.com>
2017-09-29 19:30:20 -0500

    preliminary json strings on view page

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -6,5 +6,12 @@ class DatasetsController < ApplicationController
     @debts = TrumpAdminDebts::Debt.all.to_json
     @debt_types = TrumpAdminDebts::DebtType.all.to_json
     @lenders = TrumpAdminDebts::Lender.all.to_json
+
+    @descriptor_objects = [@departments, @employees, @lenders]
+    @descriptor_titles = ['Departments', 'Employees', 'Lenders']
+
+    @numeric_objects = [@debts]
+    @numeric_titles = ['Debts']
+
   end
 end
