fccedb7ad73a4f6d5fc1fb1f770f1f86f2670815
Ida Lu <email": null>
2017-09-30 12:29:17 -0500

    Testing js connection

--- app/assets/javascripts/bar_graph.js
+++ app/assets/javascripts/bar_graph.js
@@ -1,21 +1,30 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require d3
+// = require d3
 
-var bardata = [20,30,45,15];
-var height = 400,
-    width = 600,
-    barWidth = 50,
-    barOffset = 5;
+// var bardata = [20,30,45,15];
+// var height = 400,
+//     width = 600,
+//     barWidth = 50,
+//     barOffset = 5;
 
-d3.select("#test").append('svg')
-  .attr('width', width)
-  .attr('height', height);
-  .style('background', '#C9D7D6')
-.selectAll('rect').data(bardata)
-  .enter().append('rect')
-    .style('fill','#C61C6F')
-    .attr('width', barWidth)
-    .attr('height', function(d){})
+// d3.select("#test").append('svg')
+//   .attr('width', width)
+//   .attr('height', height)
+//   .style('background', '#C9D7D6')
+// .selectAll('rect').data(bardata)
+//   .enter().append('rect')
+//     .style('fill','#C61C6F')
+//     .attr('width', barWidth)
+//     .attr('height', function(d){
+//       return d;
+//     })
+//     .attr('x', function(d){
+//       return i*(barWidth + barOffset)
+//     })
+//     .attr('y', function(d){
+//       return height -d;
+//     });
 
+d3.selectAll("h1").style("color", "yellow");
