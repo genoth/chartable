109b59cc7150cfb44c2df78be5aaf54841f6beae
Gwynne Noth <nothgwynne@gmail.com>
2017-09-29 19:12:07 -0500

    Successfully render json objects on show page, and hard-coded pie chart

---
+++ app/controllers/datasets_controller.rb
@@ -0,0 +1,14 @@
+class DatasetsController < ApplicationController
+
+  def show
+    @departments = TrumpAdminDebts::Department.all.to_json
+    @employees = TrumpAdminDebts::Employee.all.to_json
+    @debts = TrumpAdminDebts::Debt.all.to_json
+    @debt_types = TrumpAdminDebts::DebtType.all.to_json
+    @lenders = TrumpAdminDebts::Lender.all.to_json
+  end
+
+
+
+
+end
