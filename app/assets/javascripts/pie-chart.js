e46e83582ec53b56362f158740b7ea3d5258909f
Gwynne Noth <nothgwynne@gmail.com>
2017-10-01 15:24:05 -0500

    Wrap c3 generate into function

--- app/assets/javascripts/pie-chart.js
+++ app/assets/javascripts/pie-chart.js
@@ -19,12 +19,30 @@ $(document).ready(function(){
   })
 });
 
+// var chart = function(){c3.generate({
+//     data: {
+//         columns: [
+//             ['data1', 30, 200, 100, 400, 150, 250],
+//             ['data2', 50, 20, 10, 40, 15, 25]
+//         ]
+//     }
+// });
+// }
+
 var chart = function(){c3.generate({
     data: {
         columns: [
             ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ]
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
     }
 });
 }
