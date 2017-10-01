88cee4d81396fb08dc6952f0ecf26e0b5fbfa98c
Dan Ioppolo <email": null>
2017-09-30 19:06:25 -0500

    initial model tests

--- spec/controllers/datasets_controller_spec.rb
+++ spec/controllers/datasets_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DatasetsController, type: :controller do
-  let!(:debt) {Debt.create!(title: 'Imagine', artist: 'John Lennon')
+
+
+
+
 end
--- spec/models/trump_admin_debts_spec.rb
+++ spec/models/trump_admin_debts_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TrumpAdminDebts, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "form inputs" do
+    it "has the right descriptors" do
+      expect(TrumpAdminDebts.descriptors.include?("Employees")).to be true
+    end
+
+    it "has the right aggregations"
+  end
+
+  describe "query" do
+    it "has 500000 for trump's max amount when grouped by employee"
+  end
 
 
 end
