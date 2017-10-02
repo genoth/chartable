5604de6a266bc5dfc22969e8e4a6d220bf846566
Dan Ioppolo <email": null>
2017-10-02 11:19:35 -0500

    canadian temp metadata

--- db/temperature_parser.rb
+++ db/temperature_parser.rb
@@ -3,6 +3,10 @@ require_relative '../config/environment'
 
 CSV.foreach("db/TemperatureChangeSeasons_EN.csv", headers: true, header_converters: :symbol) do |row|
 
-  puts "#{row}"
+  puts row[:year]
+  puts row[:winter_temperature_celsius]
+
+
+
 
 end
