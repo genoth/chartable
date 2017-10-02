7bb275996d82091a884b537ead843f1d956d991d
Dan Ioppolo <email": null>
2017-10-02 11:50:15 -0500

    canadian temp models

---
+++ db/migrate/canadian_temp_migrations/20171002163453_year.rb
@@ -0,0 +1,15 @@
+class CreateYear < ActiveRecord::Migration[5.1]
+  def change
+
+    create_table :years do |t|
+      t.integer :year
+      t.float :winter_temperature_celsius
+      t.float :spring_temperature_celsius
+      t.float :summer_temperature_celsius
+      t.float :fall_temperature_celsius
+
+      t.timestamps
+    end
+
+  end
+end
