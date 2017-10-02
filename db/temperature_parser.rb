52725dd8847b193abacc81f80d545e03b247b3ed
Dan Ioppolo <email": null>
2017-10-02 12:12:51 -0500

    There is now temperature data

--- db/temperature_parser.rb
+++ db/temperature_parser.rb
@@ -5,8 +5,11 @@ CSV.foreach("db/TemperatureChangeSeasons_EN.csv", headers: true, header_converte
 
   puts row[:year]
   puts row[:winter_temperature_celsius]
+  puts row[:spring_temperature_celsius]
+  puts row[:summer_temperature_celsius]
+  puts row[:fall_temperature_celsius]
 
-  CanadianTempData::Year.find_or_create_by!(row)
+  CanadianTempData::Year.find_or_create_by!({year: row[:year], winter_temperature_celsius: row[:winter_temperature_celsius], spring_temperature_celsius: row[:spring_temperature_celsius], summer_temperature_celsius: row[:summer_temperature_celsius], fall_temperature_celsius: row[:fall_temperature_celsius]})
 end
 
 puts CanadianTempData::Year.count
