afc26b83725ef8a5fa4224a4178001609715c434
Dan Ioppolo <email": null>
2017-10-03 15:17:54 -0500

    begin parsing for parent tables

---
+++ db/csv_deconstruction_parent_tables.rb
@@ -0,0 +1,10 @@
+def construct_parent_table(original_csv_file, child_table_headers, child_table_data)
+
+
+
+end
+
+
+original_csv_file = 'hard_coded_filepath_string'
+child_table_headers = ['hard_coded_array']
+child_table_data = ['hard_coded_array_of_hashes']
