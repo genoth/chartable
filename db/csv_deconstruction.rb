0c07538f547b2828a4d840757a7dd40ed00761a7
Dan Ioppolo <email": null>
2017-10-03 11:52:43 -0500

    begin refactor of the writing to a new csv file

--- db/csv_deconstruction.rb
+++ db/csv_deconstruction.rb
@@ -1,5 +1,6 @@
 require 'csv'
 
+
 def construct_sub_entity(file_name, child_table_headers)
 
   child_table_data = []
@@ -7,18 +8,29 @@ def construct_sub_entity(file_name, child_table_headers)
   CSV.foreach(file_name, headers: true, header_converters: :symbol) do |original_row|
     child_table_row = Hash.new
     child_table_headers.each do |child_table_column|
-      child_table_hash.store(child_table_column, original_row[child_table_column])
-      if !child_table_data.include?(child_table_hash)
-        child_table_data << child_table_hash
+      child_table_row.store(child_table_column, original_row[child_table_column])
+      if !child_table_data.include?(child_table_row)
+        child_table_data << child_table_row
       end
     end
   end
 
-  CSV.open("test_destination.csv", "w") do |csv|
+  header_string = ""
+  child_table_headers.each_key { |header| header_string += header.to_s + ","}
+  header_string.slice!(-1)
+
+  CSV.open("db/test_destination.csv", "w") do |csv|
     child_table_data.each do |new_row|
-      csv << new_row
+      csv << header_string
+
+      #csv << new_row
     end
   end
   # write child_table_data to new CSV file.
 
 end
+
+
+filename = 'db/trump_admin_debts.csv'
+child_table_headers = [:Department, :last_name, :first_name]
+construct_sub_entity(filename, child_table_headers)
