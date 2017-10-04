b5bb2c32c86b225d4305d26149094daf50185166
Dan Ioppolo <email": null>
2017-10-04 13:24:39 -0500

    clean up notes in query file

--- app/models/canadian_temp_data/query.rb
+++ app/models/canadian_temp_data/query.rb
@@ -5,21 +5,10 @@ module CanadianClimate
     end
 
     def data
-      thing_we_want = GenderInequality.aggregation_sql_snippits[@params[:aggregations]]
-      query = GenderInequality::GenderData.select(thing_we_want + ", country").where(thing_we_want + " IS NOT null")
+      thing_we_want = CanadianClimate.aggregation_sql_snippits[@params[:aggregations]]
+      query = CanadianClimate::TempYear.select(thing_we_want + ", year").where(thing_we_want + " IS NOT null")
 
-      @dataset = query.map { |row| [row, row.country]}
-
-# this works down here
-#         query =  GenderInequality::GenderData.select("share_of_women_w_some_secondary_education_25_and_up_2005_2014, country").where("share_of_women_w_some_secondary_education_25_and_up_2005_2014 IS NOT null")
-#         @dataset = query
-
-      # # if @params[:descriptors] == "Country"
-      # #   query = GenderInequality::GenderData
-      # #     .select("#{aggregator_SQL_string}, country").where("#{aggregator_SQL_string}")
-      # #     puts "****************************************"
-      # #   p query
-      # #   @dataset = query
+      @dataset = query.map { |row| [row, row.year]}
 
       @dataset = @dataset.map { |sub_array| { label: sub_array[-1], amount: sub_array[-2][thing_we_want] } }
 
