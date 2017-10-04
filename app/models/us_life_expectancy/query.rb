136147e6a8c114f6e4b552357722a872153c703e
Gwynne Noth <nothgwynne@gmail.com>
2017-10-04 16:58:33 -0500

    Rework query class for US LifeExpectancy Module

--- app/models/us_life_expectancy/query.rb
+++ app/models/us_life_expectancy/query.rb
@@ -1,65 +1,83 @@
 module USLifeExpectancy
   class Query
+
     def initialize(params)
       @params = params
-      p params
+      @descriptors = @params[:descriptors]
+      @aggregations = @params[:aggregations]
+    end
+
+    def data
+      if @params[:chart] == "scatter"
+        scatter_data
+      end
+    end
+
+    def scatter_data
+      aggregator_SQL_string = USLifeExpectancy.aggregation_sql_snippits[@params[:aggregations]]
+      if @descriptors == "Races" # "Races" as string
+        labels = ["All Races", "Black", "White"]
+        dataset = race_descriptor_query(aggregator_SQL_string)
+        generate_c3_columns(dataset, labels)
+      elsif @descriptors == "Sexes"
+        labels = ["Both Sexes", "Female", "Male"]
+        dataset = sex_descriptor_query(aggregator_SQL_string)
+        generate_c3_columns(dataset, labels)
+      end
     end
 
 # ["Statistics", "Races", "Sexes", "Years"]
+    def race_descriptor_query(aggregator_SQL_string)
+      query = USLifeExpectancy::Statistic
+        .select("#{aggregator_SQL_string}, statistics.race_id, statistics.year_id") # race_id
+        .includes(:race) # :race
+        .includes(:year)
+        .group('statistics.race_id, statistics.year_id') # race_id
+      @dataset = query.map { |r| [r.points, r.race.race, r.year.year] } # r.race.race=
+   end
 
-    def data
-      aggregator_SQL_string = USLifeExpectancy.aggregation_sql_snippits[@params[:aggregations]]
-
-      # puts aggregator_SQL_string
-
-      if @params[:descriptors] == "Races"
-        query = USLifeExpectancy::Statistic
-            .select("#{aggregator_SQL_string}, statistics.race_id, statistics.year_id")
-            .includes(:race)
-            .includes(:year)
-            .group('statistics.race_id, statistics.year_id')
-          @dataset = query.map { |r| [r.points, r.race.race, r.year.year] }
-
-      elsif @params[:descriptors] == "Sexes"
-        query = USLifeExpectancy::Statistic
-            .select("#{aggregator_SQL_string}, statistics.sex_id, statistics.year_id")
-            .includes(:sex)
-            .includes(:year)
-            .group('statistics.sex_id, statistics.year_id')
-            p query
-          @dataset = query.map { |r| [r.points, r.sex.sex, r.year.year] }
-      end
-      x_axis = ["years_x"]
-      all_races = ["All Races"]
-      white = ["White"]
-      black = ["Black"]
-      @dataset = @dataset.sort_by { |subarray| subarray[2] }
-
-      @dataset.each do |sub_array|
-        x_axis.push(sub_array[2])
-      end
-
-      @dataset.each do |sub_array|
-        if sub_array[1] == "White"
-          white << sub_array[0]
-        elsif
-          sub_array[1] == "Black"
-          black << sub_array[0]
-        else
-          sub_array[1] == "All Races"
-          all_races << sub_array[0]
-        end
-      end
-
-      full_column_list = []
-      full_column_list.push(x_axis.uniq, white, black, all_races)
-
+    def sex_descriptor_query(aggregator_SQL_string)
+      query = USLifeExpectancy::Statistic
+          .select("#{aggregator_SQL_string}, statistics.sex_id, statistics.year_id")
+          .includes(:sex)
+          .includes(:year)
+          .group('statistics.sex_id, statistics.year_id')
+      @dataset = query.map { |r| [r.points, r.sex.sex, r.year.year] }
     end
 
-    # def prepareData(@dataset, descriptors, aggregations)
-    # end
+    def generate_c3_columns(dataset, labels)
+      x_axis = ["years_x"]
+      descriptor_both_id_1 = [labels[0]] #["All Races"]
+      descriptor_id_2 = [labels[1]] # ["Black"]
+      descriptor_id_3 = [labels[2]] # ["White"]
+      dataset = dataset.sort_by { |sub_array| sub_array.last } # sorting by Year, the x-axis value
+
+      dataset.each do |sub_array|
+        x_axis.push(sub_array[2])
+      end
+      dataset.each do |sub_array|
+        if sub_array[1] == labels[2] #"White"
+          descriptor_id_3 << sub_array[0]
+        elsif
+          sub_array[1] ==  labels[1] # "Black"
+          descriptor_id_2 << sub_array[0]
+        else
+          sub_array[1] == labels[0] # "All Races"
+          descriptor_both_id_1 << sub_array[0]
+        end
+      end
+      Array.new.push(x_axis.uniq, descriptor_both_id_1, descriptor_id_2, descriptor_id_3)
+    end
 
   end
 end
 
-
+    # def prepareData(@dataset, descriptors, aggregations)
+    # end
+    # def labels
+    #   if @descriptors == "Races"
+    #     ["All Races", "Black", "White"]
+    #   elsif @descriptors == "Sex"
+    #     ["Both Sexes", "Female", "Male"]
+    #   end
+    # end
