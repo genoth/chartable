d74ce01b4906bb806f8427822765d3f940929b9d
Dan Ioppolo <email": null>
2017-10-04 22:03:30 -0500

    more variable renaming, methods not broken

--- db/ETL_Pipeline/ETL_Parsing/parent_data_parse_out.rb
+++ db/ETL_Pipeline/ETL_Parsing/parent_data_parse_out.rb
@@ -6,28 +6,23 @@ def construct_parent_table(file_name, parent_table_headers)
   parent_table_data = []
 
   CSV.foreach(file_name, headers: true, header_converters: :symbol) do |original_row|
-    child_table_row = Hash.new
-    parent_table_headers.each do |child_table_column|
-      child_table_row.store(child_table_column, original_row[child_table_column])
-      parent_table_data << child_table_row
+    parent_table_row = Hash.new
+    parent_table_headers.each do |parent_table_column|
+      parent_table_row.store(parent_table_column, original_row[parent_table_column])
+      parent_table_data << parent_table_row
       parent_table_data.uniq!
     end
   end
 
-  # p parent_table_data
-
-
   CSV.open("db/test_destination/parent_table_test_destination.csv", "w") do |csv|
     csv << parent_table_headers
     new_row_for_csv = []
-    parent_table_data.each do |child_table_row|
+    parent_table_data.each do |parent_table_row|
       new_row_for_csv = []
-      child_table_row.each_key {|header| new_row_for_csv << child_table_row[header]}
-      # p new_row_for_csv
+      parent_table_row.each_key {|header| new_row_for_csv << parent_table_row[header]}
       csv << new_row_for_csv
     end
   end
-  # write parent_table_data to new CSV file.
 
 end
 
