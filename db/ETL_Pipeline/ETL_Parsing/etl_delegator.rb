c47471119f78709281400230dddb81a2c1386763
Dan Ioppolo <email": null>
2017-10-10 11:28:12 -0500

    ETL loop now runs, still needs to be debugged

--- db/ETL_Pipeline/ETL_Parsing/etl_delegator.rb
+++ db/ETL_Pipeline/ETL_Parsing/etl_delegator.rb
@@ -1,29 +1,40 @@
 require 'csv'
+require 'fileutils'
 require_relative 'child_data_parse_out'
 require_relative 'parent_data_parse_out'
 require_relative 'etl'
 
 
+
 pipeline_paths = [
-  "db/test_destination/one.csv",
-  "db/test_destination/two.csv",
-  "db/test_destination/three.csv",
-  "db/test_destination/four.csv",
-  "db/test_destination/five.csv",
-  "db/test_destination/six.csv",
-  "db/test_destination/seven.csv",
-  "db/test_destination/eight.csv",
-  "db/test_destination/nine.csv",
-  "db/test_destination/ten.csv",
-  "db/test_destination/eleven.csv",
+  "db/ETL_Pipeline/ETL_staging/one.csv",
+  "db/ETL_Pipeline/ETL_staging/two.csv",
+  "db/ETL_Pipeline/ETL_staging/three.csv",
+  "db/ETL_Pipeline/ETL_staging/four.csv",
+  "db/ETL_Pipeline/ETL_staging/five.csv",
+  "db/ETL_Pipeline/ETL_staging/six.csv",
+  "db/ETL_Pipeline/ETL_staging/seven.csv",
+  "db/ETL_Pipeline/ETL_staging/eight.csv",
+  "db/ETL_Pipeline/ETL_staging/nine.csv",
+  "db/ETL_Pipeline/ETL_staging/ten.csv",
+  "db/ETL_Pipeline/ETL_staging/eleven.csv",
 ]
 
 raw_data_file = "db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv"
 
 normalization_information = [
-  [ ['db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv'],[:department, :last_name, :first_name], ['db/test_destination/one.csv', 'db/test_destination/two.csv'] ],
-  [ ['db/test_destination/one.csv'],[:department], ['db/test_destination/three.csv', 'db/test_destination/four.csv'] ], # 3=departments, 4=employees
-  [ ['db/test_destination/two.csv'],[:lender], ['db/test_destination/five.csv', 'db/test_destination/six.csv'] ], # 5=lenders
-  [ ['db/test_destination/six.csv'],[:debt_type], ['db/test_destination/seven.csv', 'db/test_destination/eight.csv'] ] # 7=debt_types, 8=debts
+  ['db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv',[:department, :last_name, :first_name], 'db/ETL_Pipeline/ETL_staging/one.csv', 'db/ETL_Pipeline/ETL_staging/two.csv'],
+  ['db/ETL_Pipeline/ETL_staging/one.csv',[:department], 'db/ETL_Pipeline/ETL_staging/three.csv', 'db/ETL_Pipeline/ETL_staging/four.csv'], # 3=departments, 4=employees
+  ['db/ETL_Pipeline/ETL_staging/two.csv',[:lender],'db/ETL_Pipeline/ETL_staging/five.csv', 'db/ETL_Pipeline/ETL_staging/six.csv'], # 5=lenders
+  ['db/ETL_Pipeline/ETL_staging/six.csv',[:debt_type],'db/ETL_Pipeline/ETL_staging/seven.csv', 'db/ETL_Pipeline/ETL_staging/eight.csv'] # 7=debt_types, 8=debts
 ]
+
+normalization_information.each do |files|
+  split_tables(files[0], files[1], files[2], files[3])
+  if files[0] != 'db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv'
+    FileUtils.mv("#{files[0]}", "db/ETL_Pipeline/ETL_trash")
+  end
+end
+
+
 
--- db/ETL_Pipeline/ETL_test_destination/filler.rb
+++
@@ -1,1 +0,0 @@
-# This file is in the ETL test destination folder
