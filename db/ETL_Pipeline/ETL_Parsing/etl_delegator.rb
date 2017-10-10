f1fbfad973db2dbee0396b19b8d246cbdcd8257e
Dan Ioppolo <email": null>
2017-10-10 10:19:51 -0500

    clean up child table parsing

--- db/ETL_Pipeline/ETL_Parsing/etl_delegator.rb
+++ db/ETL_Pipeline/ETL_Parsing/etl_delegator.rb
@@ -1,6 +1,6 @@
 require 'csv'
-require 'child_data_parse_out'
-require 'parent_data_parse_out'
+#require_relative 'child_data_parse_out'
+#require_relative 'parent_data_parse_out'
 
 pipeline_paths = [
   "db/test_destination/one.csv",
@@ -19,8 +19,9 @@ pipeline_paths = [
 raw_data_file = "db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv"
 
 normalization_information = [
-  [ [filepath_0],[department, last_name, first_name], [filepath_1, filepath_2] ],
-  [ [filepath_1],[department], [filepath_3, filepath_4] ], # 3=departments, 4=employees
-  [ [filepath_2],[lender], [filepath_5, filepath_6] ], # 5=lenders
-  [ [filepath_6],[debt_type], [filepath_7, filepath_8] ] # 7=debt_types, 8=debts
+  [ ['db/ETL_Pipeline/raw_CSVs/trump_admin_debts.csv'],[:department, :last_name, :first_name], ['db/test_destination/one.csv', 'db/test_destination/two.csv'] ],
+  [ ['db/test_destination/one.csv'],[:department], ['db/test_destination/three.csv', 'db/test_destination/four.csv'] ], # 3=departments, 4=employees
+  [ ['db/test_destination/two.csv'],[:lender], ['db/test_destination/five.csv', 'db/test_destination/six.csv'] ], # 5=lenders
+  [ ['db/test_destination/six.csv'],[:debt_type], ['db/test_destination/seven.csv', 'db/test_destination/eight.csv'] ] # 7=debt_types, 8=debts
 ]
+
