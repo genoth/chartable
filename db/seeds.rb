886c4987bf1c4284155361ceda2fba01e3e5f0c7
genoth <nothgwynne@gmail.com>
2017-10-16 12:35:29 -0500

    Add police killings csv and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,9 @@ require_relative 'ETL_Pipeline/ETL_Parsing/general_parser'
 
   #dataset_label: {raw_csv_filepath: '/filepath_string', dataset_db_destination: 'namespace_table_name'},
 autoparsable_datasets = [
- {"raw_csv_filepath" => "db/ETL_Pipeline/raw_CSVs/gender_inequality.csv", "dataset_db_destination" => GenderInequality::GenderData},
-  {"raw_csv_filepath" => "db/ETL_Pipeline/raw_CSVs/temperature-change-seasons.csv", "dataset_db_destination" => CanadianClimate::TempYear}
+  {"raw_csv_filepath"} => "db/ETL_Pipeline/raw_CSVs/police_killings.csv", "dataset_db_destination" => PoliceKillings::Deaths}
+ # {"raw_csv_filepath" => "db/ETL_Pipeline/raw_CSVs/gender_inequality.csv", "dataset_db_destination" => GenderInequality::GenderData},
+ #  {"raw_csv_filepath" => "db/ETL_Pipeline/raw_CSVs/temperature-change-seasons.csv", "dataset_db_destination" => CanadianClimate::TempYear}
 ]
 #general_parser("db/ETL_Pipeline/raw_CSVs/gender_inequality.csv", GenderInequality::GenderData)
 #general_parser("db/ETL_Pipeline/raw_CSVs/temperature-change-seasons.csv", CanadianClimate::TempYear)
@@ -26,54 +27,54 @@ end
 
 # Trump Debts
 
-counter_t = 0
-CSV.foreach("db/ETL_Pipeline/raw_CSVs/updated_trump_admin_debts.csv", headers: true, header_converters: :symbol) do |row|
-  counter_t += 1
-  print 't' if counter_t % 100 == 0
+# counter_t = 0
+# CSV.foreach("db/ETL_Pipeline/raw_CSVs/updated_trump_admin_debts.csv", headers: true, header_converters: :symbol) do |row|
+#   counter_t += 1
+#   print 't' if counter_t % 100 == 0
 
-  department = TrumpAdminDebts::Department.find_or_create_by!(name: row[:department])
-  employee = TrumpAdminDebts::Employee.find_or_create_by!(last_name: row[:last_name], first_name: row[:first_name], department: department)
-  lender = TrumpAdminDebts::Lender.find_or_create_by!(name: row[:lender_name])
-  debt_type = TrumpAdminDebts::DebtType.find_or_create_by!(description: row[:debt_type])
+#   department = TrumpAdminDebts::Department.find_or_create_by!(name: row[:department])
+#   employee = TrumpAdminDebts::Employee.find_or_create_by!(last_name: row[:last_name], first_name: row[:first_name], department: department)
+#   lender = TrumpAdminDebts::Lender.find_or_create_by!(name: row[:lender_name])
+#   debt_type = TrumpAdminDebts::DebtType.find_or_create_by!(description: row[:debt_type])
 
-  min_amount = (row[:min_amount]).to_i
-  if row[:max_amount]
-    max_amount = (row[:max_amount]).to_i
-  end
+#   min_amount = (row[:min_amount]).to_i
+#   if row[:max_amount]
+#     max_amount = (row[:max_amount]).to_i
+#   end
 
-  debt = TrumpAdminDebts::Debt.find_or_create_by!({
-    employee: employee,
-    lender: lender,
-    debt_type: debt_type,
-    min_amount: min_amount,
-    max_amount: max_amount,
-    year_incurred: row[:year_incurred],
-    rate: row[:rate],
-    term: row[:term]
-    })
-end
+#   debt = TrumpAdminDebts::Debt.find_or_create_by!({
+#     employee: employee,
+#     lender: lender,
+#     debt_type: debt_type,
+#     min_amount: min_amount,
+#     max_amount: max_amount,
+#     year_incurred: row[:year_incurred],
+#     rate: row[:rate],
+#     term: row[:term]
+#     })
+# end
 
-# US Races Life Expectancy
+# # US Races Life Expectancy
 
-# DI note: there is an interesting ETL issue here - Year.first is the last year(2015) and Year.last is the first year(1900). Would be really cool if our system could intelligently determine which order to read the lines in based on that.
-counter_r = 0
-CSV.foreach("db/ETL_Pipeline/raw_CSVs/birth-death-rate.csv", headers: true, header_converters: :symbol) do |row|
-  counter_r += 1
-  print 'r' if counter_r % 100 == 0
+# # DI note: there is an interesting ETL issue here - Year.first is the last year(2015) and Year.last is the first year(1900). Would be really cool if our system could intelligently determine which order to read the lines in based on that.
+# counter_r = 0
+# CSV.foreach("db/ETL_Pipeline/raw_CSVs/birth-death-rate.csv", headers: true, header_converters: :symbol) do |row|
+#   counter_r += 1
+#   print 'r' if counter_r % 100 == 0
 
-  race = USLifeExpectancy::Race.find_or_create_by!(race: row[:race])
-  sex = USLifeExpectancy::Sex.find_or_create_by!(sex: row[:sex])
-  year = USLifeExpectancy::Year.find_or_create_by!(year: row[:year])
+#   race = USLifeExpectancy::Race.find_or_create_by!(race: row[:race])
+#   sex = USLifeExpectancy::Sex.find_or_create_by!(sex: row[:sex])
+#   year = USLifeExpectancy::Year.find_or_create_by!(year: row[:year])
 
-  age_adjusted_death_rate = row[:age_adjusted_death_rate]
-  average_life_expectancy = row[:average_life_expectancy_in_years]
+#   age_adjusted_death_rate = row[:age_adjusted_death_rate]
+#   average_life_expectancy = row[:average_life_expectancy_in_years]
 
-  statistic = USLifeExpectancy::Statistic.find_or_create_by!({
-    year: year,
-    race: race,
-    sex: sex,
-    age_adjusted_death_rate: age_adjusted_death_rate,
-    average_life_expectancy: average_life_expectancy
-    })
+#   statistic = USLifeExpectancy::Statistic.find_or_create_by!({
+#     year: year,
+#     race: race,
+#     sex: sex,
+#     age_adjusted_death_rate: age_adjusted_death_rate,
+#     average_life_expectancy: average_life_expectancy
+#     })
 
-end
+# end
